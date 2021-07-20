1dc1bfde514a57e69058fa7541c3724ea4d1de8a
noeul1114 <parkhyongsok@naver.com>
2021-07-20 11:25:04 +0900

    7/20 4ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import NewModel
 
@@ -41,6 +41,12 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = UserCreationForm
+    context_object_name = 'target_user'
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
 
 
 
