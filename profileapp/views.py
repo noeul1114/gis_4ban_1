3f21bf47f5172c419615928f9a9adda70d03104d
noeul1114 <parkhyongsok@naver.com>
2021-07-28 17:36:23 +0900

    7/27 class 4ban commit 4

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
@@ -19,13 +24,13 @@ class ProfileCreateView(CreateView):
         return super().form_valid(form)
 
 
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
     context_object_name = 'target_profile'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
-
-
 
 
