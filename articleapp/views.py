35265a5b16363d29faf54e14a893582c501fa736
noeul1114 <parkhyongsok@naver.com>
2021-08-06 11:44:09 +0900

    8/6 class 4ban commit 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -53,5 +53,10 @@ class ArticleDeleteView(DeleteView):
     template_name = 'articleapp/delete.html'
 
 
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 20
 
 
