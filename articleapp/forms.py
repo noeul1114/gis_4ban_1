92b19c7e6e760ec8c82360d94f8105eabd8ccf9e
noeul1114 <parkhyongsok@naver.com>
2021-08-25 17:42:35 +0900

    8/25 class 4ban commit 3

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,14 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
 
 
 class ArticleCreationForm(ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'editable',
+                                                           'style': 'min-height: 10rem;'
+                                                                    'text-align: left;'}))
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
